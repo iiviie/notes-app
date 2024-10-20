da458e6b87130f5187a67cf5397f45406a926374
iiviie <divyanshverma158@gmail.com>
2024-10-20 21:02:38 +0530

    add CRUD ops to views

--- notesObsidian/notes/views.py
+++ notesObsidian/notes/views.py
@@ -1,5 +1,45 @@
 from django.shortcuts import render
+from .models import Note
+from .forms import NoteForm
+from django.shortcuts import get_object_or_404 , redirect
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def note_list(request):
+    notes = Note.objects.all().order_by('-created_at')
+    return render(request, 'notes/note_list.html', {'notes': notes})
+
+def note_create(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST, request.FILES)
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.user = request.user
+            note.save()
+            return redirect('note_list')
+    else:
+        form = NoteForm()
+    return render(request, 'notes/note_form.html', {'form': form})
+
+def note_edit(request, note_id):
+    note = get_object_or_404(note,pk=note_id, user = request.user)
+    if request.method == 'POST':
+        form = NoteForm(request.POST, request.FILES, instance=note)
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.user = request.user
+            note.save()
+            return redirect('note_list')
+    else:
+        form = NoteForm(instance=note)
+    return render(request, 'notes/note_form.html', {'form': form})
+
+def note_delete(request, note_id):
+    note = get_object_or_404(Note, pk=note_id, user=request.user)
+    if request.method == 'POST':
+        note.delete()
+        return redirect('note_list')
+    return render(request, 'notes/note_confirm_delete.html', {'note': note})
+
