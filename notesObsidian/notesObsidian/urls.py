4ac7e274f66f7b12394ae5589b07af36e620daac
iiviie <divyanshverma158@gmail.com>
2024-10-21 19:35:48 +0530

    fix base URL

--- notesObsidian/notesObsidian/urls.py
+++ notesObsidian/notesObsidian/urls.py
@@ -19,9 +19,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.urls import views as auth_views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('notes/', include('notes.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+     path('', RedirectView.as_view(url='/notes/', permanent=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
